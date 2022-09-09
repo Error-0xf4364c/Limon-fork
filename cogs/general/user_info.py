03db01246d9c404460a66c27676d62a582278da0
Abdurrahman Coşar <80622960+abdurrahmancosar@users.noreply.github.com>
2022-09-09 18:49:12 +0300

    Update user_info.py

--- cogs/general/user_info.py
+++ cogs/general/user_info.py
@@ -145,4 +145,4 @@ class general(commands.Cog, commands.Bot):
 
 
 async def setup(bot:commands.Bot):
-    await bot.add_cog(general(bot), guilds= [discord.Object(id =964617424743858176)])
+    await bot.add_cog(general(bot))
