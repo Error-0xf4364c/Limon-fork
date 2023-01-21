80ddf9ffdef91ef4b3240d89405cc132d28f1a94
Abduley <80622960+AbdurrahmanCosar@users.noreply.github.com>
2023-01-21 18:36:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
- * Cupcake Bot for Discord
+ * Limon Bot for Discord
  * Copyright (C) 2022 Abdurrahman Coşar
  * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
