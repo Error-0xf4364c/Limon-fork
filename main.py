d3e36ef9b66e03cc908698dc18d3fab489782585
Abduley <80622960+AbdurrahmanCosar@users.noreply.github.com>
2023-01-25 11:17:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
  * Limon Bot for Discord
- * Copyright (C) 2022 Abdurrahman Coşar
+ * Copyright (C) 2022 AbdurrahmanCosar
  * This software is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
  * For more information, see README.md and LICENSE
 """
